49d42a725dbc2010f2c351da1dfc94a5161a73c4
Chris Mems <christopher.mems@.com>
2023-02-17 16:48:36 -0600

    create a post route for adding a workout

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-const express = require('express')
-
 require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.path, res.method)
   next()
 })
 
 // routes
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome'})
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(() => {
+  // listen for requests
+  app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+  })
+
+})
+.catch((error) => {
+  console.log(error)
 })
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port 4000')
-})
