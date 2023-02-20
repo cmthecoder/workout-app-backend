046b217913c67bb8aed124d1a5d510dc7465d163
Chris Mems <christopher.mems@.com>
2023-02-20 10:26:36 -0600

    generate token after signing up

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,10 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 const loginUser = async (req, res) => {
   res.json({mess: 'login user'})
@@ -11,7 +17,10 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password)
-    res.status(200).json({email, user})
+
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
